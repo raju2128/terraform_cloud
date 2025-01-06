183e34ac3db4fdf960d83dc87ed17bc5bc47aa0b
Raju_2128 <108755755+raju2128@users.noreply.github.com>
2025-01-06 11:31:20 +0000

    updated

--- main.tf
+++ main.tf
@@ -1,1 +1,12 @@
+provider "aws" {
+  region  = "us-east-1"
+}
 
+resource "aws_instance" "webserver" {
+  ami           = "ami-0e2c8caa4b6378d8c"
+  instance_type = "t2.micro"
+  key_name = "devops"
+  tags = {
+    Name = "webserver"
+  }
+}
